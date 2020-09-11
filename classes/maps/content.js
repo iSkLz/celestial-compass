d9324b27b53b211a8493aa0590c261c82bce6f4f
iSkLz <isklzcode@gmail.com>
2020-09-11 11:29:01 +0100

    Some code

---
+++ classes/maps/content.js
@@ -0,0 +1,20 @@
+exports.Entity = Entity;
+exports.Entity = Solid;
+exports.Entity = Tileset;
+exports.Entity = Trigger;
+
+class Entity {
+
+}
+
+class Solid {
+
+}
+
+class Tileset {
+
+}
+
+class Trigger {
+
+}
